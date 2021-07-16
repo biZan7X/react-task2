da38274811f22946cbca6319716b4f4b2bdd3119
Biswanath Tewari <biswanath@geekyants.com>
2021-07-16 14:42:41 +0530

    feat - time display

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todays date and time</h1>
+      <h2>{new Date().getDate()}th July</h2>
+      <h2>{new Date().toLocaleTimeString()}</h2>
     </div>
   );
 }
